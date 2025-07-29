42a0c74d2e8915288962f9401690317fbf9d24db
InnocentusLime <innocentuslime.help@gmail.com>
2025-07-29 19:41:41 +0300

    quad-col: cleanup group and world tests

--- quad-col/tests/collider_group.rs
+++ quad-col/tests/collider_group.rs
@@ -1,10 +1,85 @@
+mod common;
+
+use common::{TestCase, draw_shape, run_tests_no_fuzz};
 use glam::{Affine2, Mat2, vec2};
 use hashbrown::HashSet;
 use quad_col::{Collider, Group, Shape};
 
+#[derive(Debug, Clone, Copy)]
+struct GroupTest {
+    name: &'static str,
+    expected: &'static [usize],
+    group: Group,
+}
+
+impl GroupTest {
+    fn get_query(&self) -> Collider {
+        Collider {
+            shape: Shape::Circle { radius: 10.0 },
+            group: self.group,
+            tf: Affine2::from_translation(vec2(0.0, 0.0)),
+        }
+    }
+}
+
+impl TestCase for GroupTest {
+    fn name(&self) -> &'static str {
+        self.name
+    }
+
+    fn check(&self) -> bool {
+        let query = self.get_query();
+        let matched = (0..CIRCLE_COUNT)
+            .filter(move |idx| CIRCLES[*idx].collides(&query))
+            .collect::<HashSet<_>>();
+        let expected = self.expected.iter().map(|x| *x).collect::<HashSet<_>>();
+
+        matched == expected
+    }
+
+    fn draw(&self, canvas: &mut image::RgbImage) {
+        let colors = [
+            image::Rgb([255, 0, 0]),
+            image::Rgb([0, 255, 0]),
+            image::Rgb([0, 0, 255]),
+        ];
+        for (circle, color) in CIRCLES.into_iter().zip(colors) {
+            draw_shape(canvas, color, circle.shape, circle.tf);
+        }
+
+        let query = self.get_query();
+        draw_shape(canvas, image::Rgb([255, 255, 255]), query.shape, query.tf);
+    }
+}
+
+#[test]
+fn test_group_intersections() {
+    run_tests_no_fuzz(tests());
+}
+
+fn tests() -> impl IntoIterator<Item = GroupTest> {
+    [
+        GroupTest {
+            name: "group[1, 2]",
+            expected: &[1, 2],
+            group: Group::from_id(1).union(Group::from_id(2)),
+        },
+        GroupTest {
+            name: "group[0, 2]",
+            expected: &[0, 2],
+            group: Group::from_id(0).union(Group::from_id(2)),
+        },
+        GroupTest {
+            name: "group[0, 1]",
+            expected: &[0, 1],
+            group: Group::from_id(0).union(Group::from_id(1)),
+        },
+    ]
+}
+
 const CIRCLE: Shape = Shape::Circle { radius: 2.0 };
 const CIRCLE_COUNT: usize = 3;
-static CIRCLE_MATRIX: [Collider; CIRCLE_COUNT] = [
+static CIRCLES: [Collider; CIRCLE_COUNT] = [
     Collider {
         shape: CIRCLE,
         tf: Affine2 {
@@ -30,36 +105,3 @@ static CIRCLE_MATRIX: [Collider; CIRCLE_COUNT] = [
         group: Group::from_id(2),
     },
 ];
-static QUERY_MATRIX: [Collider; CIRCLE_COUNT] = [
-    Collider {
-        group: Group::from_id(1).union(Group::from_id(2)),
-        ..CIRCLE_MATRIX[0]
-    },
-    Collider {
-        group: Group::from_id(0).union(Group::from_id(2)),
-        ..CIRCLE_MATRIX[1]
-    },
-    Collider {
-        group: Group::from_id(0).union(Group::from_id(1)),
-        ..CIRCLE_MATRIX[2]
-    },
-];
-
-fn get_collisions(query: Collider) -> impl Iterator<Item = usize> {
-    (0..CIRCLE_COUNT).filter(move |idx| CIRCLE_MATRIX[*idx].collides(&query))
-}
-
-#[test]
-fn test_group_intersections() {
-    let expected = [
-        HashSet::from_iter([1, 2]),
-        HashSet::from_iter([0, 2]),
-        HashSet::from_iter([0, 1]),
-    ];
-    assert_eq!(expected.len(), CIRCLE_COUNT);
-    for (idx, query) in QUERY_MATRIX.into_iter().enumerate() {
-        let expected = &expected[idx];
-        let found = get_collisions(query).collect::<HashSet<_>>();
-        assert_eq!(expected, &found);
-    }
-}
--- quad-col/tests/world.rs
+++ quad-col/tests/world.rs
@@ -1,169 +1,182 @@
+mod common;
+
+use common::{TestCase, draw_shape, run_tests_no_fuzz};
 use glam::{Affine2, Mat2, vec2};
 use hashbrown::HashSet;
-use hecs::World;
+use hecs::{Entity, World};
 use quad_col::{Collider, CollisionSolver, Group, Shape};
 
-const CIRCLE: Shape = Shape::Circle { radius: 2.0 };
-const CIRCLE_COUNT: usize = 3;
-static CIRCLE_MATRIX: [Collider; CIRCLE_COUNT] = [
-    Collider {
-        shape: CIRCLE,
-        tf: Affine2 {
-            translation: vec2(0.0, 1.5),
-            matrix2: Mat2::IDENTITY,
+#[derive(Debug, Clone, Copy)]
+struct WorldTest {
+    name: &'static str,
+    expected: &'static [usize],
+    circle_groups: [Group; CIRCLE_COUNT],
+    query_group: Group,
+    query_filter: Group,
+}
+
+impl WorldTest {
+    fn get_query(&self) -> (Collider, Group) {
+        let col = Collider {
+            shape: Shape::Circle { radius: 10.0 },
+            group: self.query_group,
+            tf: Affine2::from_translation(vec2(0.0, 0.0)),
+        };
+
+        (col, self.query_filter)
+    }
+
+    fn spawn_entities(&self, world: &mut World) -> Vec<Entity> {
+        CIRCLES
+            .into_iter()
+            .zip(self.circle_groups)
+            .map(|(collider, group)| Collider { group, ..collider })
+            .map(ColliderComponent)
+            .map(|x| world.spawn((x,)))
+            .collect::<Vec<_>>()
+    }
+
+    fn fill_solver(&self, world: &mut World, solver: &mut CollisionSolver) {
+        solver.fill(
+            world
+                .query_mut::<&ColliderComponent>()
+                .into_iter()
+                .map(|(entity, component)| (entity, component.0)),
+        );
+    }
+}
+
+impl TestCase for WorldTest {
+    fn name(&self) -> &'static str {
+        self.name
+    }
+
+    fn check(&self) -> bool {
+        let mut world = World::new();
+        let mut solver = CollisionSolver::new();
+        let spawned = self.spawn_entities(&mut world);
+        self.fill_solver(&mut world, &mut solver);
+
+        let (col, filter) = self.get_query();
+        let expected = self
+            .expected
+            .into_iter()
+            .map(|idx| spawned[*idx])
+            .collect::<HashSet<_>>();
+        let actual = solver
+            .query_overlaps(col, filter)
+            .map(|(entitiy, _)| *entitiy)
+            .collect::<HashSet<_>>();
+
+        actual == expected
+    }
+
+    fn draw(&self, canvas: &mut image::RgbImage) {
+        let colors = [
+            image::Rgb([255, 0, 0]),
+            image::Rgb([0, 255, 0]),
+            image::Rgb([0, 0, 255]),
+        ];
+        for (circle, color) in CIRCLES.into_iter().zip(colors) {
+            draw_shape(canvas, color, circle.shape, circle.tf);
+        }
+
+        let (query, _) = self.get_query();
+        draw_shape(canvas, image::Rgb([255, 255, 255]), query.shape, query.tf);
+    }
+}
+
+#[test]
+fn test_world() {
+    run_tests_no_fuzz(tests());
+}
+
+fn tests() -> impl IntoIterator<Item = WorldTest> {
+    [
+        WorldTest {
+            name: "empty",
+            expected: &[],
+            circle_groups: [Group::from_id(0), Group::from_id(1), Group::from_id(2)],
+            query_group: Group::empty(),
+            query_filter: Group::empty(),
         },
-        group: Group::from_id(0),
-    },
-    Collider {
-        shape: CIRCLE,
-        tf: Affine2 {
-            translation: vec2(1.0, -1.0),
-            matrix2: Mat2::IDENTITY,
+        WorldTest {
+            name: "group[1, 2]",
+            expected: &[1, 2],
+            circle_groups: [Group::from_id(0), Group::from_id(1), Group::from_id(2)],
+            query_group: pair_group(1, 2),
+            query_filter: Group::empty(),
         },
-        group: Group::from_id(1),
-    },
-    Collider {
-        shape: CIRCLE,
-        tf: Affine2 {
-            translation: vec2(-1.0, -1.0),
-            matrix2: Mat2::IDENTITY,
+        WorldTest {
+            name: "group[0, 2]",
+            expected: &[0, 2],
+            circle_groups: [Group::from_id(0), Group::from_id(1), Group::from_id(2)],
+            query_group: pair_group(0, 2),
+            query_filter: Group::empty(),
         },
-        group: Group::from_id(2),
-    },
-];
-static CIRCLE_MATRIX_HARD: [Collider; CIRCLE_COUNT] = [
-    Collider {
-        shape: CIRCLE,
-        tf: Affine2 {
-            translation: vec2(0.0, 1.5),
-            matrix2: Mat2::IDENTITY,
+        WorldTest {
+            name: "group[0, 1]",
+            expected: &[0, 1],
+            circle_groups: [Group::from_id(0), Group::from_id(1), Group::from_id(2)],
+            query_group: pair_group(0, 1),
+            query_filter: Group::empty(),
         },
-        group: Group::from_id(1).union(Group::from_id(2)),
-    },
-    Collider {
-        shape: CIRCLE,
-        tf: Affine2 {
-            translation: vec2(1.0, -1.0),
-            matrix2: Mat2::IDENTITY,
+        WorldTest {
+            name: "group[1, 2] & filter[1, 2]",
+            expected: &[0],
+            circle_groups: [pair_group(1, 2), pair_group(0, 2), pair_group(0, 1)],
+            query_group: pair_group(1, 2),
+            query_filter: pair_group(1, 2),
         },
-        group: Group::from_id(0).union(Group::from_id(2)),
-    },
-    Collider {
-        shape: CIRCLE,
-        tf: Affine2 {
-            translation: vec2(-1.0, -1.0),
-            matrix2: Mat2::IDENTITY,
+        WorldTest {
+            name: "group[0, 2] & filter[0, 2]",
+            expected: &[1],
+            circle_groups: [pair_group(1, 2), pair_group(0, 2), pair_group(0, 1)],
+            query_group: pair_group(0, 2),
+            query_filter: pair_group(0, 2),
         },
-        group: Group::from_id(0).union(Group::from_id(1)),
-    },
-];
-static QUERY_MATRIX: [Collider; CIRCLE_COUNT] = [
-    Collider {
-        group: Group::from_id(1).union(Group::from_id(2)),
-        ..CIRCLE_MATRIX[0]
-    },
-    Collider {
-        group: Group::from_id(0).union(Group::from_id(2)),
-        ..CIRCLE_MATRIX[1]
-    },
-    Collider {
-        group: Group::from_id(0).union(Group::from_id(1)),
-        ..CIRCLE_MATRIX[2]
-    },
-];
-static FILTER_MATRIX: [Group; CIRCLE_COUNT] = [
-    Group::from_id(1).union(Group::from_id(2)),
-    Group::from_id(0).union(Group::from_id(2)),
-    Group::from_id(0).union(Group::from_id(1)),
-];
+        WorldTest {
+            name: "group[0, 1] & filter[0, 1]",
+            expected: &[2],
+            circle_groups: [pair_group(1, 2), pair_group(0, 2), pair_group(0, 1)],
+            query_group: pair_group(0, 1),
+            query_filter: pair_group(0, 1),
+        },
+    ]
+}
+
+fn pair_group(x: u32, y: u32) -> Group {
+    Group::from_id(x).union(Group::from_id(y))
+}
 
 #[repr(transparent)]
 struct ColliderComponent(Collider);
 
-#[test]
-fn test_world_empty() {
-    let mut world = World::new();
-    for circle in CIRCLE_MATRIX {
-        world.spawn((ColliderComponent(circle),));
-    }
-    let mut solver = CollisionSolver::new();
-    let it = world.query_mut::<&ColliderComponent>();
-    solver.fill(
-        it.into_iter()
-            .map(|(entity, component)| (entity, component.0)),
-    );
-    let overlaps = solver
-        .query_overlaps(
-            Collider {
-                tf: Affine2::IDENTITY,
-                shape: Shape::Circle { radius: 64.0 },
-                group: Group::empty(),
-            },
-            Group::empty(),
-        )
-        .collect::<Vec<_>>();
-    assert!(overlaps.is_empty())
-}
-
-#[test]
-fn test_world_simple() {
-    let mut world = World::new();
-    let spawned = CIRCLE_MATRIX
-        .into_iter()
-        .map(ColliderComponent)
-        .map(|c| world.spawn((c,)))
-        .collect::<Vec<_>>();
-    let expected = [
-        HashSet::from_iter([spawned[1], spawned[2]]),
-        HashSet::from_iter([spawned[0], spawned[2]]),
-        HashSet::from_iter([spawned[0], spawned[1]]),
-    ];
-    assert_eq!(expected.len(), CIRCLE_COUNT);
-    let mut solver = CollisionSolver::new();
-    let it = world.query_mut::<&ColliderComponent>();
-    solver.fill(
-        it.into_iter()
-            .map(|(entity, component)| (entity, component.0)),
-    );
-
-    for (idx, query) in QUERY_MATRIX.into_iter().enumerate() {
-        let expected = &expected[idx];
-        let (got_ent, _) = solver
-            .query_overlaps(query, Group::empty())
-            .map(|(e, c)| (*e, *c))
-            .collect::<(HashSet<_>, Vec<_>)>();
-        assert_eq!(&got_ent, expected);
-    }
-}
-
-#[test]
-fn test_world_with_filters() {
-    let mut world = World::new();
-    let spawned = CIRCLE_MATRIX_HARD
-        .into_iter()
-        .map(ColliderComponent)
-        .map(|c| world.spawn((c,)))
-        .collect::<Vec<_>>();
-    let expected = [
-        HashSet::from_iter([spawned[0]]),
-        HashSet::from_iter([spawned[1]]),
-        HashSet::from_iter([spawned[2]]),
-    ];
-    assert_eq!(expected.len(), CIRCLE_COUNT);
-    let mut solver = CollisionSolver::new();
-    let it = world.query_mut::<&ColliderComponent>();
-    solver.fill(
-        it.into_iter()
-            .map(|(entity, component)| (entity, component.0)),
-    );
-
-    for (idx, query) in QUERY_MATRIX.into_iter().enumerate() {
-        let filter = FILTER_MATRIX[idx];
-        let expected = &expected[idx];
-        let (got_ent, _) = solver
-            .query_overlaps(query, filter)
-            .map(|(e, c)| (*e, *c))
-            .collect::<(HashSet<_>, Vec<_>)>();
-        assert_eq!(&got_ent, expected);
-    }
-}
+const CIRCLE: Shape = Shape::Circle { radius: 2.0 };
+const CIRCLE_COUNT: usize = 3;
+static CIRCLES: [Collider; CIRCLE_COUNT] = [
+    Collider {
+        shape: CIRCLE,
+        tf: Affine2 {
+            translation: vec2(0.0, 1.5),
+            matrix2: Mat2::IDENTITY,
+        },
+        group: Group::empty(),
+    },
+    Collider {
+        shape: CIRCLE,
+        tf: Affine2 {
+            translation: vec2(1.0, -1.0),
+            matrix2: Mat2::IDENTITY,
+        },
+        group: Group::empty(),
+    },
+    Collider {
+        shape: CIRCLE,
+        tf: Affine2 {
+            translation: vec2(-1.0, -1.0),
+            matrix2: Mat2::IDENTITY,
+        },
+        group: Group::empty(),
+    },
+];
