8fa5343ba4b52921d58cbcc60d36a7143189a001
InnocentusLime <innocentuslime.help@gmail.com>
2025-08-15 13:04:40 +0300

    lib-level: use anyhow for error reporting
    Let's make lib-levels error stuff more usable and user-friendly.
    This also makes thiserror a dev-only depedency.

--- lib-level/src/binary_io.rs
+++ lib-level/src/binary_io.rs
@@ -1,13 +1,9 @@
-use crate::{LoadLevelError, level::LevelDef};
-
-use std::error::Error as StdError;
+use crate::level::LevelDef;
 
 /// Loads a level from memory, treating the bytes as the binary level
 /// format encoding. For internal use only.
-pub fn load_from_memory(data: &[u8]) -> Result<LevelDef, LoadLevelError> {
-    postcard::from_bytes(data)
-        .map_err(|e| Box::new(e) as Box<dyn StdError>)
-        .map_err(LoadLevelError::Loading)
+pub fn load_from_memory(data: &[u8]) -> anyhow::Result<LevelDef> {
+    postcard::from_bytes(data).map_err(Into::into)
 }
 
 /// Level compilation routine. For internal use only.
@@ -16,13 +12,11 @@ pub mod compile {
     use super::LevelDef;
     use postcard::{ser_flavors::io::WriteFlavor, serialize_with_flavor};
 
-    use std::error::Error as StdError;
     use std::io::Write;
 
     /// Write the level to `out` in binary format.
-    pub fn write_level(level: &LevelDef, out: impl Write) -> Result<(), Box<dyn StdError>> {
-        serialize_with_flavor(level, WriteFlavor::new(out))
-            .map(|_| ())
-            .map_err(|e| Box::new(e) as Box<dyn StdError>)
+    pub fn write_level(level: &LevelDef, out: impl Write) -> anyhow::Result<()> {
+        serialize_with_flavor(level, WriteFlavor::new(out))?;
+        Ok(())
     }
 }
