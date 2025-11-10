72d6471d6763de685b2539448b918e47f86d0eb2
InnocentusLime <innocentuslime.help@gmail.com>
2025-11-10 22:06:48 +0200

    game: move col constants to lib-game

--- src/game/prelude.rs
+++ src/game/prelude.rs
@@ -4,28 +4,3 @@ pub use lib_game::*;
 pub use macroquad::prelude::*;
 
 pub const TILE_SIDE_F32: f32 = lib_level::TILE_SIDE as f32;
-
-pub mod col_group {
-    use lib_col::Group;
-
-    pub const NONE: Group = Group::empty();
-    pub const LEVEL: Group = Group::from_id(0);
-    pub const PLAYER: Group = Group::from_id(1);
-    #[allow(dead_code)]
-    pub const ENEMY: Group = Group::from_id(2);
-    pub const DAMAGABLE: Group = Group::from_id(3);
-}
-
-pub mod col_query {
-    pub const LEVEL: usize = 0;
-    pub const DAMAGE: usize = 1;
-    pub const PICKUP: usize = 2;
-    pub const INTERACTION: usize = 3;
-
-    #[allow(dead_code)]
-    pub type Level = lib_game::CollisionQuery<LEVEL>;
-    pub type Damage = lib_game::CollisionQuery<DAMAGE>;
-    pub type Pickup = lib_game::CollisionQuery<PICKUP>;
-    #[allow(dead_code)]
-    pub type Interaction = lib_game::CollisionQuery<INTERACTION>;
-}
