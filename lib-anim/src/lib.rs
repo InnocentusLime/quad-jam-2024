e807d7dff4f1d485a959bed3547d45e7aa53779b
InnocentusLime <innocentuslime.help@gmail.com>
2025-12-07 16:47:42 +0200

    lib-anim: add animation pack fallback

--- lib-anim/src/lib.rs
+++ lib-anim/src/lib.rs
@@ -20,8 +20,9 @@ impl AnimationPackId {
         self,
         resolver: &FsResolver,
     ) -> anyhow::Result<HashMap<AnimationId, Animation>> {
-        use log::info;
+        use log::warn;
         use std::path::PathBuf;
+        use strum::VariantArray;
 
         let mut filename = PathBuf::from(self.get_filename());
         filename.set_extension("json");
@@ -29,14 +30,39 @@ impl AnimationPackId {
         let aseprite_path = resolver.aseprite_path(&filename);
         let project_path = resolver.animation_pack_proj_path(&filename);
 
+        match aseprite_load::load_animations_project(&project_path) {
+            Ok(x) => return Ok(x),
+            Err(e) => warn!("Failed to load anim pack {self:?}: {e:?}"),
+        }
+
         // On native (dev-environment) we load from aseprite and project files.
         // First we try to load the project. If that fails, we try to load aseprite.
         // This way it is faster to iterate on designs.
-        match aseprite_load::load_animations_project(&project_path) {
+        match aseprite_load::load_animations_aseprite(resolver, &aseprite_path) {
             Ok(x) => return Ok(x),
-            Err(e) => info!("Failed to load {project_path:?}: {e:?}"),
+            Err(e) => warn!("Failed to load aseprite sheet {self:?}: {e:?}"),
         }
-        aseprite_load::load_animations_aseprite(resolver, aseprite_path)
+
+        warn!("Animationed pack {self:?} will be replaced with a placeholder");
+        let placeholder = AnimationId::VARIANTS
+            .into_iter()
+            .filter(|x| {
+                let anim_name: &'static str = (*x).into();
+                let pack_name: &'static str = (&self).into();
+                anim_name.starts_with(pack_name)
+            })
+            .map(|x| {
+                (
+                    *x,
+                    Animation {
+                        is_looping: true,
+                        clips: vec![],
+                        tracks: vec![],
+                    },
+                )
+            })
+            .collect();
+        Ok(placeholder)
     }
 
     #[cfg(target_family = "wasm")]
