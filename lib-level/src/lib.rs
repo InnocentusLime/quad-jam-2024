55a372eca885dabd91ffc87d7a0c03d1de41b977
InnocentusLime <innocentuslime.help@gmail.com>
2025-12-14 10:20:25 +0200

    lib-level: fully migrate to lib-asset

--- lib-level/src/lib.rs
+++ lib-level/src/lib.rs
@@ -4,20 +4,30 @@ mod level;
 pub mod tiled_load;
 
 pub use level::*;
-use lib_asset::FsResolver;
+use lib_asset::{FsResolver, declare_assets};
 
-/// Loads a level by name. This is the public API for use inside the
-/// game.
-pub async fn load_level(resolver: &FsResolver, name: &str) -> anyhow::Result<LevelDef> {
+declare_assets!(
+    LevelId(level_filename, level_path) {
+        TestRoom("test_room.bin"),
+    }
+);
+
+impl LevelId {
     #[cfg(not(target_family = "wasm"))]
-    return tiled_load::load_level_by_name(resolver, name);
-    #[cfg(target_family = "wasm")]
-    return load_level_release(resolver, name).await;
-}
+    pub async fn load_level(self, resolver: &FsResolver) -> anyhow::Result<LevelDef> {
+        use std::path::PathBuf;
 
-#[cfg(target_family = "wasm")]
-async fn load_level_release(_resolver: &FsResolver, name: &str) -> anyhow::Result<LevelDef> {
-    use macroquad::prelude::*;
-    let data = load_file(&format!("levels/{name}.bin")).await?;
-    binary_io::load_from_memory(&data).map_err(Into::into)
+        let mut filename = PathBuf::from(self.get_filename());
+        filename.set_extension("tmx");
+
+        tiled_load::load_level(resolver, resolver.tiled_path(filename))
+    }
+
+    #[cfg(target_family = "wasm")]
+    pub async fn load_level(self, resolver: &FsResolver) -> anyhow::Result<LevelDef> {
+        use macroquad::prelude::*;
+        let path = self.resolve(resolver);
+        let data = load_file(path.to_str().unwrap()).await?;
+        binary_io::load_from_memory(&data)
+    }
 }
