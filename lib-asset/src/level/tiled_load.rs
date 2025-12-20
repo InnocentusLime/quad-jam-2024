ed6b4d0dd9faf8ac463b16a358f9ef6ba149231d
InnocentusLime <innocentuslime.help@gmail.com>
2025-12-20 17:05:53 +0200

    lib-asset/level: simplify error handling

--- lib-asset/src/level/tiled_load.rs
+++ lib-asset/src/level/tiled_load.rs
@@ -1,18 +1,15 @@
 //! This module contains logic for decoding [LevelDef] from a `Tiled`
 //! map file.
 
-use std::{
-    fs, io,
-    ops::Deref,
-    path::{Path, PathBuf},
-};
+use std::fs;
+use std::path::Path;
 
 use crate::{FsResolver, TextureId};
+use anyhow::Context;
 use hashbrown::HashMap;
 use macroquad::texture::Texture2D;
-use thiserror::Error;
 
-use super::tiled_props_des::{DeserializerError, from_properties};
+use super::tiled_props_des::from_properties;
 use crate::level::*;
 
 /// Load a level by path through tield. For internal use only.
@@ -24,49 +21,49 @@ pub fn load_level(resolver: &FsResolver, path: impl AsRef<Path>) -> anyhow::Resu
     Ok(level)
 }
 
-fn load_level_from_map(
-    resolver: &FsResolver,
-    map: &tiled::Map,
-) -> Result<LevelDef, LoadFromTiledError> {
-    if map.version() != REQUIRED_TILED_VERSION {
-        return Err(LoadFromTiledError::UnsupportedVersion(
-            map.version().to_string(),
-        ));
-    }
-    if map.tile_height != TILE_SIDE {
-        return Err(LoadFromTiledError::TileHeight(map.tile_height));
-    }
-    if map.tile_width != TILE_SIDE {
-        return Err(LoadFromTiledError::TileWidth(map.tile_width));
-    }
-    if map.tilesets().len() != 1 {
-        return Err(LoadFromTiledError::UnexpectedTilesetAmount(
-            map.tilesets().len(),
-        ));
-    }
+fn load_level_from_map(resolver: &FsResolver, map: &tiled::Map) -> anyhow::Result<LevelDef> {
+    anyhow::ensure!(
+        map.version() == REQUIRED_TILED_VERSION,
+        "Unsupported tiled map version: {:?}",
+        map.version(),
+    );
+    anyhow::ensure!(
+        map.tile_height == TILE_SIDE,
+        "Incorrect tile height: expected {TILE_SIDE:}, found {0:}",
+        map.tile_height,
+    );
+    anyhow::ensure!(
+        map.tile_width == TILE_SIDE,
+        "Incorrect tile width: expected {TILE_SIDE:}, found {0:}",
+        map.tile_width,
+    );
+    anyhow::ensure!(
+        map.tilesets().len() == 1,
+        "Expected the map to have 1 tileset. Found {0:?}",
+        map.tilesets().len(),
+    );
 
     let width = map.width;
     let height = map.height;
     let mut layers_by_name = HashMap::<String, tiled::Layer>::new();
     for layer in map.layers() {
         let name = layer.name.clone();
-        if name != OBJECT_LAYER && name != WORLD_LAYER {
-            return Err(LoadFromTiledError::UnknownLayer(name));
-        }
+        anyhow::ensure!(
+            name == OBJECT_LAYER || name == WORLD_LAYER,
+            "Unknown layer: {name:?}",
+        );
 
         let old = layers_by_name.insert(name.clone(), layer);
-        if old.is_some() {
-            return Err(LoadFromTiledError::DuplicateLayer(name));
-        }
+        anyhow::ensure!(old.is_none(), "Duplicate layer: {name:?}",);
     }
 
     let Some(mapdef_layer) = layers_by_name.get(WORLD_LAYER) else {
-        return Err(LoadFromTiledError::WorldLayerAbsent);
+        anyhow::bail!("World layer {WORLD_LAYER:?} not found");
     };
     let map = load_mapdef_from_layer(resolver, mapdef_layer, width, height)?;
 
     let Some(entitydefs_layer) = layers_by_name.get(OBJECT_LAYER) else {
-        return Err(LoadFromTiledError::ObjectLayerAbsent);
+        anyhow::bail!("Object layer {OBJECT_LAYER:?} not found");
     };
     let entities = load_entity_defs_from_object_layer(entitydefs_layer)?;
 
@@ -82,49 +79,37 @@ fn load_mapdef_from_layer(
     layer: &tiled::Layer,
     map_width: u32,
     map_height: u32,
-) -> Result<MapDef, LoadFromTiledError> {
+) -> anyhow::Result<MapDef> {
     let Some(tile_layer) = layer.as_tile_layer() else {
-        return Err(LoadFromTiledError::WorldLayerNotTileLayer);
+        anyhow::bail!("Expected layer {WORLD_LAYER:?} to be a tile layer");
     };
     let Some(layer_width) = tile_layer.width() else {
-        return Err(LoadFromTiledError::WorldLayerInfinite);
+        anyhow::bail!("Expected layer {WORLD_LAYER:?} to be a finite tile layer");
     };
     let Some(layer_height) = tile_layer.height() else {
-        return Err(LoadFromTiledError::WorldLayerInfinite);
+        anyhow::bail!("Expected layer {WORLD_LAYER:?} to be a finite tile layer");
     };
-    if layer_width != map_width {
-        return Err(LoadFromTiledError::WorldLayerWidth {
-            expected: map_width,
-            found: layer_width,
-        });
-    }
-    if layer_height != map_height {
-        return Err(LoadFromTiledError::WorldLayerHeight {
-            expected: map_height,
-            found: layer_height,
-        });
-    }
+    anyhow::ensure!(
+        layer_width == map_width,
+        "Incorrect layer {WORLD_LAYER:?} width: expected {map_width}, found {layer_width}",
+    );
+    anyhow::ensure!(
+        layer_height == map_height,
+        "Incorrect layer {WORLD_LAYER:?} height: expected {map_height}, found {layer_height}",
+    );
 
-    let tileset = layer.map().tilesets()[0].deref();
+    let tileset = &*layer.map().tilesets()[0];
     let mut tiles = HashMap::<_, Tile>::new();
     for (tile_idx, tile_data) in tileset.tiles() {
-        let parsed_tile_idx =
-            TileIdx::from_repr(tile_idx).ok_or_else(|| LoadFromTiledError::UnknownTile {
-                tileset: tileset.name.clone(),
-                tile_idx,
-            })?;
-        let tile = from_properties(TILE_CLASS, &tile_data.properties).map_err(|reason| {
-            LoadFromTiledError::TileDeserError {
-                tileset: tileset.name.clone(),
-                tile_idx,
-                reason,
-            }
+        let parsed_tile_idx = TileIdx::from_repr(tile_idx).ok_or_else(|| {
+            anyhow::anyhow!("Tileset {:?}, tile {tile_idx:}: unknown tile", tileset.name,)
         })?;
-
+        let tile = from_properties(TILE_CLASS, &tile_data.properties)
+            .with_context(|| format!("Tileset {:?}, tile {tile_idx}", tileset.name))?;
         tiles.insert(parsed_tile_idx, tile);
     }
     let Some(tileset_atlas) = tileset.image.as_ref() else {
-        return Err(LoadFromTiledError::MapTilesetIrregular);
+        anyhow::bail!("Image collection based tilesets are not supported");
     };
     let atlas = resolve_atlas(resolver, &tileset.name, &tileset_atlas.source)?;
 
@@ -133,12 +118,9 @@ fn load_mapdef_from_layer(
         for x in 0..layer_width {
             let tile_idx = match tile_layer.get_tile(x as i32, y as i32) {
                 None => TileIdx::Empty,
-                Some(t) => {
-                    TileIdx::from_repr(t.id()).ok_or_else(|| LoadFromTiledError::UnknownTile {
-                        tileset: tileset.name.clone(),
-                        tile_idx: t.id(),
-                    })?
-                }
+                Some(t) => TileIdx::from_repr(t.id()).ok_or_else(|| {
+                    anyhow::anyhow!("Tileset {:?}, tile {:}: unknown tile", t.id(), tileset.name,)
+                })?,
             };
             tilemap.push(tile_idx);
         }
@@ -155,33 +137,26 @@ fn load_mapdef_from_layer(
     })
 }
 
-fn load_entity_defs_from_object_layer(
-    layer: &tiled::Layer,
-) -> Result<Vec<EntityDef>, LoadFromTiledError> {
+fn load_entity_defs_from_object_layer(layer: &tiled::Layer) -> anyhow::Result<Vec<EntityDef>> {
     let Some(object_layer) = layer.as_object_layer() else {
-        return Err(LoadFromTiledError::ObjectLayerNotObjectLayer);
+        anyhow::bail!("Expected layer {OBJECT_LAYER:?} to be an object layer")
     };
 
     let mut entities = Vec::new();
     for object in object_layer.objects() {
-        if object.user_type == "" {
-            return Err(LoadFromTiledError::NoObjectType {
-                obj_idx: object.id(),
-            });
-        }
-        let info = from_properties(&object.user_type, &object.properties).map_err(|reason| {
-            LoadFromTiledError::ObjectDeserError {
-                obj_idx: object.id(),
-                reason,
-            }
-        })?;
+        anyhow::ensure!(
+            object.user_type != "",
+            "Layer {OBJECT_LAYER:?}, object {}: no class",
+            object.id(),
+        );
+        let info = from_properties(&object.user_type, &object.properties)
+            .with_context(|| format!("Layer {OBJECT_LAYER:?}, object {}", object.id()))?;
         let (width, height) = match object.shape {
             tiled::ObjectShape::Rect { width, height } => (width, height),
-            _ => {
-                return Err(LoadFromTiledError::NonSquareObject {
-                    obj_idx: object.id(),
-                });
-            }
+            _ => anyhow::bail!(
+                "Layer {OBJECT_LAYER:?}, object {}: non square object",
+                object.id(),
+            ),
         };
 
         entities.push(EntityDef {
@@ -205,78 +180,16 @@ fn resolve_atlas(
     resolver: &FsResolver,
     tileset: &str,
     atlas_path: impl AsRef<Path>,
-) -> Result<TextureId, LoadFromTiledError> {
-    let atlas_path = fs::canonicalize(atlas_path.as_ref()).map_err(|reason| {
-        LoadFromTiledError::TilesetImageNotCanonizable {
-            tileset: tileset.to_string(),
-            path: atlas_path.as_ref().into(),
-            reason,
-        }
-    })?;
-    let id = resolver.inverse_resolve::<Texture2D>(&atlas_path).unwrap();
-
-    Ok(id)
+) -> anyhow::Result<TextureId> {
+    let atlas_path = atlas_path.as_ref();
+    let atlas_path = fs::canonicalize(atlas_path)
+        .with_context(|| format!("Canonicalizing tileset {tileset:?} atlas {atlas_path:?}"))?;
+    resolver
+        .inverse_resolve::<Texture2D>(&atlas_path)
+        .with_context(|| format!("Inverse resolving {atlas_path:?}"))
 }
 
 static REQUIRED_TILED_VERSION: &'static str = "1.10";
 static OBJECT_LAYER: &'static str = "Actors";
 static WORLD_LAYER: &'static str = "World";
 static TILE_CLASS: &'static str = "Tile";
-
-#[derive(Debug, Error)]
-pub enum LoadFromTiledError {
-    #[error("Unsupported tiled map version: {0:?}")]
-    UnsupportedVersion(String),
-    #[error("Incorrect tile height: expected {TILE_SIDE:}, found {0:}")]
-    TileHeight(u32),
-    #[error("Incorrect tile width: expected {TILE_SIDE:}, found {0:}")]
-    TileWidth(u32),
-    #[error("Duplicate layer: {0:?}")]
-    DuplicateLayer(String),
-    #[error("Unknown layer: {0:?}")]
-    UnknownLayer(String),
-    #[error("World layer ({WORLD_LAYER:?}) not found")]
-    WorldLayerAbsent,
-    #[error("Object layer ({OBJECT_LAYER:?}) not found")]
-    ObjectLayerAbsent,
-    #[error("Expected layer {WORLD_LAYER:?} to be a tile layer")]
-    WorldLayerNotTileLayer,
-    #[error("Expected world layer to be finite")]
-    WorldLayerInfinite,
-    #[error("Incorrect layer {WORLD_LAYER:?} width: expected {expected:}, found {found:}")]
-    WorldLayerWidth { expected: u32, found: u32 },
-    #[error("Incorrect layer {WORLD_LAYER:?} height: expected {expected:}, found {found:}")]
-    WorldLayerHeight { expected: u32, found: u32 },
-    #[error("Expected the map to have 1 tileset. Found {0:?}")]
-    UnexpectedTilesetAmount(usize),
-    #[error("Image collection based tilesets are not supported")]
-    MapTilesetIrregular,
-    #[error("Tileset {tileset:?}: path {path:?} can't be resolved")]
-    TilesetImageNotCanonizable {
-        tileset: String,
-        path: PathBuf,
-        #[source]
-        reason: io::Error,
-    },
-    #[error("Tileset {tileset:?}, tile {tile_idx:}: unknown tile")]
-    UnknownTile { tileset: String, tile_idx: u32 },
-    #[error("Tileset {tileset:?}, tile {tile_idx:}: failed to deserialize properties")]
-    TileDeserError {
-        tileset: String,
-        tile_idx: u32,
-        #[source]
-        reason: DeserializerError,
-    },
-    #[error("Expected layer {OBJECT_LAYER:?} to be an object layer")]
-    ObjectLayerNotObjectLayer,
-    #[error("Object {obj_idx}: no class")]
-    NoObjectType { obj_idx: u32 },
-    #[error("Object {obj_idx}: failed to deserialize properties")]
-    ObjectDeserError {
-        obj_idx: u32,
-        #[source]
-        reason: DeserializerError,
-    },
-    #[error("Object {obj_idx}: expected shape to be square, found")]
-    NonSquareObject { obj_idx: u32 },
-}
